c857a81f8bf82f0783aeb1187a6b227a11b753ff
Ricardo Delgado <ridel007@gmail.com>
2020-12-20 08:36:29 -0500

    Register User Test Case

--- src/test/java/com/rockcor/test/RegisterTest.java
+++ src/test/java/com/rockcor/test/RegisterTest.java
@@ -5,6 +5,7 @@ package com.rockcor.test;
 
 import static org.junit.Assert.*;
 
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 import org.junit.After;
@@ -12,7 +13,9 @@ import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+
 
 /**
  * @author Rockcor
@@ -27,8 +30,8 @@ public class RegisterTest {
 	private final static By REGISTER_PAGE_LOC = By.xpath("//img[@src='images/mast_register.gif']");
 	private final static By REGISTER_EMAIL_LOC = By.id("email");
 	private final static By REGISTER_PASSWORD_LOC = By.name("password");
-	private final static By REGISTER_CONF_PASSWORD_LOC = By.name("confirmPassword");
-	//private final static By REGISTER_CONF_PASSWORD_LOC = By.cssSelector("input[name='confirmPassword']");
+	//private final static By REGISTER_CONF_PASSWORD_LOC = By.name("confirmPassword");
+	private final static By REGISTER_CONF_PASSWORD_LOC = By.cssSelector("input[name='confirmPassword']");
 	private final static By REGISTER_SUBMIT = By.name("submit");
 	
 	private final static String USERNAME = "qualityAdmin123!=";
@@ -63,7 +66,7 @@ public class RegisterTest {
 		//Escenario 1: Registrar un usuario
 		//Paso 1: Clic en Register
 		driver.findElement(REGISTER_LINK_LOC).click();
-		Thread.sleep(10000);
+		Thread.sleep(2000);
 		
 		if( driver.findElement(REGISTER_PAGE_LOC).isDisplayed() ) {
 			System.out.print("Encontrado");
@@ -72,6 +75,18 @@ public class RegisterTest {
 			driver.findElement(REGISTER_PASSWORD_LOC).sendKeys(PASSWORD);
 			driver.findElement(REGISTER_CONF_PASSWORD_LOC).sendKeys(PASSWORD);
 			driver.findElement(REGISTER_SUBMIT).click();
+			
+			List<WebElement> fonts = driver.findElements(By.tagName("font"));
+			
+			String resultS = "Note: Your user name is " + USERNAME + ".";
+			
+			for (WebElement webElement : fonts) {
+				System.out.println(fonts.indexOf(webElement) + " - " + webElement.getText());
+			}
+			
+			System.out.println(fonts.get(5).getText());
+			
+			assertEquals(resultS, fonts.get(5).getText());
 			
 		} else {
 			System.out.print("Register page was not found");
